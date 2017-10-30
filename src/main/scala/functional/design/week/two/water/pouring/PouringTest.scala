e71212d2e6e15eabf08f52ec7e2761708f3c733b
andrew6000 <ag91375@gmail.com>
2017-10-30 18:37:11 -0400

    update...

---
+++ src/main/scala/functional/design/week/two/water/pouring/PouringTest.scala
@@ -0,0 +1,14 @@
+package functional.design.week.two.water.pouring
+
+import functional.design.week.two.water.pouring._
+
+object PouringTest {
+
+  def main(args: Array[String]): Unit ={
+
+    val p =  Pouring(Vector(12, 8, 5), Vector(12, 0, 0))
+    val result = p.solution(Vector(6, 6, 0)).toList
+    println(result)
+  }
+
+}
