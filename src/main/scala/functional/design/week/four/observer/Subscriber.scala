86b924edb6cee8e8d25770b3294aa08e94e6bc96
andrew6000 <ag91375@gmail.com>
2017-11-09 13:16:40 -0500

    update...

--- src/main/scala/functional/design/week/four/observer/Subscriber.scala
+++ src/main/scala/functional/design/week/four/observer/Subscriber.scala
@@ -1,5 +1,10 @@
 package functional.design.week.four.observer
 
+
+/*Subscribers have a simple all they need
+to have is this handler method, and we pass
+the publisher that published new information
+as a parameter to that handler.*/
 trait Subscriber {
 
   def handler(pub: Publisher)
