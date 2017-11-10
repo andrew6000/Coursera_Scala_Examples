b555db6fb6c802b59d508ab75af224a128ec4ff6
andrew6000 <ag91375@gmail.com>
2017-11-10 15:30:52 -0500

    update...

--- src/main/scala/functional/design/week/three/des/functional/design/week/three/des/circut/gates/Simulation.scala
+++ src/main/scala/functional/design/week/three/des/functional/design/week/three/des/circut/gates/Simulation.scala
@@ -40,28 +40,70 @@ trait Simulation {
   do with the actual wall clock time. It is the simulated time.
 
   */
+  //type action that takes an empty parameter list to unit.
   type Action = () => Unit
-  
+
+
+ /* Then we would have a class of events.
+
+  For convenience, we make it into a case class.
+
+  That way it's easy to match that an event would have a time when
+  it should be executed
+
+  and an Action which gives us the function that should be executed.
+  */
   case class Event(time: Int, action: Action)
-  
-  private type Agenda = List[Event]
+
+
+/*The principle idea is to keep in every instance
+  of that trait an agenda of actions to perform.
+
+  The agenda would be a list of simulated events, and each event would consist
+  of an action and the time when that action should be produced.
+
+  We sort the agenda list in such a way that the actions to be performed first
+  are in the beginning, that way we can simply pick them off the front of the
+  list to execute them.
+  */
+  private type Agenda = List[Event] // just a list of events
   private var agenda: Agenda = List()
-  
-  //to handle time
+  // variable agenda of type Agenda, which is initially the empty list.
+
+  /*To handle time, we do that with another private variable, call it curtime,
+    that contains the current simulation time, and that one can then be accessed
+    with the getter function currentTime.
+  */
   private var curtime = 0 //returns the current simulated time in the form of an integer.
   def currentTime: Int = curtime
 
 
-/*function afterDelay:
+/*function afterDelay: given delay and a given block
+
   Can install a block of statements to be performed as an action
   at a time that is delay time units after the current time.*/
-  def afterDelay(delay: Int)(block: => Unit): Unit = {   
+  def afterDelay(delay: Int)(block: => Unit): Unit = {
+    /*
+    creates the event at the given time with the given actions to perform
+    */
     val item = Event(currentTime + delay, () => block)
+    //inserts it into the agenda
     agenda = insert(agenda, item)
   }
   
-  //Ensures the agenda is time-sorted
+  /*
+  Ensures the agenda is time-sorted
+
+  insert an event at the right place in the list that it appears at the
+  right position given its time.*/
   private def insert(agenda: List[Event], item: Event): List[Event] = agenda match {
+
+ /* A pattern match on the agenda.
+
+  If the agenda has a first element that has a time that is less or equal to
+  the current item's time, then we insert the item in the rest of the list,
+  because we should still keep first as the first element.
+    */
     case first :: rest if(first.time <= item.time) => first :: insert(rest, item)
     case _ => item :: agenda
   }
