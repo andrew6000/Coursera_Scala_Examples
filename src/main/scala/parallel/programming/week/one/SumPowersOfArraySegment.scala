5c3519b4e79eecd58d43a3c37664a943b4090e4f
andrew6000 <ag91375@gmail.com>
2017-11-24 16:03:25 -0500

    update...

--- src/main/scala/parallel/programming/week/one/SumPowersOfArraySegment.scala
+++ src/main/scala/parallel/programming/week/one/SumPowersOfArraySegment.scala
@@ -23,6 +23,18 @@ object SumPowersOfArraySegment {
     result
   }
 
+  /*
+  * The time bound on sequential execution time of this function is
+    linear in the difference between t and s because we have an array and
+    we are processing elements from s to t.
+
+    For each element of the array, we are taking the constant amount of time
+    which gives us some linear function of the form c1 (t- s) + c2. This constants
+    c1 and c2 may be important in practice but are irrelevant for the asymptotic analysis,
+    which only looks at the rate growth of the function describing the performance.
+
+    W(s, t) = O(t - s)
+  * */
   def sumSegment(a: Array[Int], p: Double, s: Int, t: Int): Int = {
     var i=s;
     var sum: Int = 0
@@ -31,7 +43,48 @@ object SumPowersOfArraySegment {
       i +=1
     }
     sum
-  }                                         //> sumSegment2: (a: Array[Int], p: Double, s: Int, t: Int)Int
+  }
+
+  val threshold = 2
+
+
+  def segmentRec(a: Array[Int], p: Double, s: Int, t: Int):Int = {
+    /*when the difference between the bounds is below some threshold,
+    then we will invoke our previous sumSegment function */
+    if (t - s < threshold) {
+      sumSegment(a, p, s, t)
+    } else {
+      /*We will compute the middle element of the array and then invoke
+      ourselves recursively with the intervals s to m and m to t.
+
+      running time of the recursive function can be described using a computation tree
+
+      the computation reduces to two recursive calls, one that takes parameters s and m1
+      where m1 is the middle. And the other one takes parameters m1 and t.
+
+      And this decomposition continues until the length of our segment
+      is below the threshold. So computation over recursive function
+      can be described using such tree.
+      */
+      val m= s + (t - s)/2
+      val (sum1, sum2)= (segmentRec(a, p, s, m), segmentRec(a, p, m, t))
+      sum1 + sum2
+    }
+  }
+
+
+ /*
+  def segmentRec(a: Array[Int], p: Double, s: Int, t: Int): Int = {
+    if(t-s < threshold)
+      sumSegment(a,p,s,t)
+    else {
+      val m = s + (t-s)/2
+      val (sum1, sum2) = parallel(segmentRec(a,p,s,m), segmentRec(a,p,m,t))
+
+      sum1+sum2
+    }
+  }*/
+
   def power(x: Int, p: Double): Int = math.exp(p*math.log(math.abs(x))).toInt
 
 
@@ -65,17 +118,7 @@ object SumPowersOfArraySegment {
     power(part1+part2+part3+part4, 1/p)
   }
 
-  val threshold = 2
-  def segmentRec(a: Array[Int], p: Double, s: Int, t: Int): Int = {
-    if(t-s < threshold)
-      sumSegment(a,p,s,t)
-    else {
-      val m = s + (t-s)/2
-      val (sum1, sum2) = parallel(segmentRec(a,p,s,m), segmentRec(a,p,m,t))
 
-      sum1+sum2
-    }
-  }
 
   def pNormRec(a:Array[Int], p:Double): Int = power(segmentRec(a,p,0,a.length),1/p)
 }
