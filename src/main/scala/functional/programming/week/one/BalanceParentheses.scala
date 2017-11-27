8ebe82783ee4311d95bbbb9b88c6106e1905cd5d
andrew6000 <ag91375@gmail.com>
2017-11-26 19:40:12 -0500

    Modifying for ScalaMeter

---
+++ src/main/scala/functional/programming/week/one/BalanceParentheses.scala
@@ -0,0 +1,26 @@
+package functional.programming.week.one
+
+import scala.annotation.tailrec
+
+object BalanceParentheses {
+
+ def balance1(chars: Array[Char]): Unit ={
+
+   @tailrec
+   def balance(chars: Array[Char], count:Int = 0): Boolean = (chars, count) match {
+     case (cs, 0)  if cs.isEmpty => true
+     case (cs, _)  if cs.isEmpty => false
+     case (cs, c)  => cs.head match {
+       case '(' => balance(cs.tail, c+1)
+       case ')' if c > 0 => balance(cs.tail, c-1)
+       case ')' => false
+       case _  => balance(cs.tail, c)
+     }
+   }
+
+   balance(chars,0)
+ }
+
+
+
+}
