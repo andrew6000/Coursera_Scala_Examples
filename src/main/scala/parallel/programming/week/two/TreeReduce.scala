92ba056f84f752963f876a2e6d3a641e89fce428
andrew6000 <ag91375@gmail.com>
2017-11-28 16:27:25 -0500

    update...

--- src/main/scala/parallel/programming/week/two/TreeReduce.scala
+++ src/main/scala/parallel/programming/week/two/TreeReduce.scala
@@ -2,7 +2,18 @@ package parallel.programming.week.two
 
 object TreeReduce {
 
-  sealed abstract class Tree[A]
+  sealed abstract class Tree[A]{
+
+    def toList: List[A] = this match {
+      case Leaf(v) => List(v)
+      case Node(l, r) => l.toList ++ r.toList
+    }
+
+    def map[B](f: A => B): Tree[B] = this match {
+      case Leaf(v) => Leaf(f(v))
+      case Node(l,r) => Node(l.map(f), r.map(f))
+    }
+  }
   case class Leaf[A](value: A) extends Tree[A]
   case class Node[A](l: Tree[A], r: Tree[A]) extends Tree[A] {
     override def toString = s"{" + l + " | " + r + "}"
@@ -22,6 +33,7 @@ object TreeReduce {
   }
 
   object Tree {
+
     def apply[A](v: A): Leaf[A] = new Leaf(v)
     def apply[A](l: Tree[A], r: Tree[A]): Tree[A] = new Node(l, r)
   }
