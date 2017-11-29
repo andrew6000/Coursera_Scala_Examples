5429ef9ebbd724dfbc42d87b96d016b81c661788
andrew6000 <ag91375@gmail.com>
2017-11-29 18:00:01 -0500

    update

--- src/main/scala/parallel/programming/week/two/AssociativeOperationsOnTuples.scala
+++ src/main/scala/parallel/programming/week/two/AssociativeOperationsOnTuples.scala
@@ -19,6 +19,13 @@ object AssociativeOperationsOnTuples {
 
   def f(a1: (A1, A2), a2: (A1, A2)) = (f1(a1._1, a2._1), f2(a1._2, a2._2))
 
+  def x = 10
+  def y = 9
+  def z = 8
+
+  def f3(x:Int,y:Int) = x+y
+
+
   def main(args:Array[String]): Unit ={
 
     val basicAssociativity = f((x1, y1), (x2, y2)) == (f1(x1, x2), f2(y1, y2))
@@ -39,5 +46,22 @@ object AssociativeOperationsOnTuples {
 
     println(basicAssociativity == deepAssociativity)
     assert(basicAssociativity == deepAssociativity)
+
+ /*   Although commutativity of f alone does not imply associativity, it implies
+      it if we have an additional property. Define:
+      E(x,y,z) = f(f(x,y), z)
+    We say arguments of E can rotate if E(x,y,z) = E(y,z,x), that is:
+      f(f(x,y), z) = f(f(y,z), x)
+    Claim: if f is commutative and arguments of E can rotate then f is also
+    associative.
+      Proof:
+      f(f(x,y), z) = f(f(y,z), x) = f(x, f(y,z))*/
+    val associativeIfCommutative = f3(f3(x,y), z) == f3(f3(y,z), x)
+    println(associativeIfCommutative)
+    assert(associativeIfCommutative)
+
+    val tripleProof = (f3(f3(x,y), z) == f3(f3(y,z), x) == f3(x, f3(y,z)) )
+    println(tripleProof)
+    assert(tripleProof)
   }
 }
