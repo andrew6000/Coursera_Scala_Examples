e24531fbb4831c5f536f87d20e797d44377c6056
andrew6000 <ag91375@gmail.com>
2017-11-30 14:17:28 -0500

    update

--- src/main/scala/parallel/programming/week/two/ParallelMapOverArray.scala
+++ src/main/scala/parallel/programming/week/two/ParallelMapOverArray.scala
@@ -18,4 +18,16 @@ object ParallelMapOverArray {
     }
   }
 
+  def mapASegPar2[A,B](inp: Array[A], left: Int, right: Int,
+                      f: (A,A) => B, out: Array[B]): Unit = {
+    //Write to out(i) for left <= i <= right - 1
+    if(right - left < threshold) {  //Threshold needs to be large enough,
+      // otherwise we lose efficiency
+      SequentialMapOverArray.mapASegSeq2(inp, left, right, f, out)
+    } else {
+      val mid = left + (right-left) / 2
+      parallel(mapASegPar2(inp, left, mid, f, out), mapASegPar2(inp, mid, right, f, out))
+    }
+  }
+
 }
