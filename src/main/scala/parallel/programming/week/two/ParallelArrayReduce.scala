76f292c4d7c870cc1f8bf8bdce1d5df2dda32205
andrew6000 <ag91375@gmail.com>
2017-11-29 21:54:09 -0500

    update

--- src/main/scala/parallel/programming/week/two/ParallelArrayReduce.scala
+++ src/main/scala/parallel/programming/week/two/ParallelArrayReduce.scala
@@ -1,5 +1,7 @@
 package parallel.programming.week.two
 
+import scala.annotation.tailrec
+
 object ParallelArrayReduce {
 
   val threshold = 10000
@@ -23,6 +25,29 @@ object ParallelArrayReduce {
     }
   }
 
-  def reduce[A](a: Array[A], f: (A, A) => A): A = reduceSeg(a, 0, a.length, f)
+  def reduceSeg2[A](a: Array[A], left: Int, right: Int,
+                   f: (A, A) => A): A = {
+
+    if(right - left < threshold) {
+      var res = a(left)
+      var i = left + 1
+      while(i < right) {
+        res = f(res, a(i))
+        i += 1
+      }
+      res
+    } else {
+      val mid = left + (right - left) / 2
+      /*val (a1, a2) = parallel(reduceSeg(a, left, mid, f),
+        reduceSeg(a, mid, right, f))*/
+      val a1 = task{reduceSeg2(a, left, mid, f)}
+      val a2 =reduceSeg2(a, mid, right, f)
+      a1.join()
+
+      f(a1.get(), a2)
+    }
+  }
+
+  def reduce[A](a: Array[A], f: (A, A) => A): A = reduceSeg2(a, 0, a.length, f)
 
 }
