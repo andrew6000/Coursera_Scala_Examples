3c3200005aff3bd5a9f9401f07e89e9a2149d6dd
andrew6000 <ag91375@gmail.com>
2017-11-30 12:24:39 -0500

    update

---
+++ src/test/scala/parallel/programming/week/two/ScanLeftBenchmarkTest.scala
@@ -0,0 +1,40 @@
+package parallel.programming.week.two
+
+import org.scalameter._
+
+object ScanLeftBenchmarkTest {
+
+  val standardConfig = config(
+    Key.exec.minWarmupRuns -> 30,
+    Key.exec.maxWarmupRuns -> 100,
+    Key.exec.benchRuns -> 100,
+    Key.verbose -> true
+  )
+
+  def initializeArray(xs: Array[Int]) {
+    var i = 0
+    while (i < xs.length) {
+      xs(i) = i
+      i += 1
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    def fPlus = (x: Int, y: Int) => x + y
+    val length = 70000
+    val xs = new Array[Int](length)
+
+    val seqtime = standardConfig setUp {
+      _ => val list = initializeArray(xs)
+    } measure {
+
+      val out = new Array[Int](length+1)
+      SequentialScanLeft.scanLeftSeq(xs,0,fPlus,out)
+    }
+
+    println(s"sequential time: $seqtime ms")
+
+  }
+
+}
