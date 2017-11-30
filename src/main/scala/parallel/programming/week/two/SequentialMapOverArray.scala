8e03415dcf3a913e1b037efcbf09a5021ff89ac4
andrew6000 <ag91375@gmail.com>
2017-11-30 14:17:05 -0500

    update

--- src/main/scala/parallel/programming/week/two/SequentialMapOverArray.scala
+++ src/main/scala/parallel/programming/week/two/SequentialMapOverArray.scala
@@ -13,4 +13,14 @@ object SequentialMapOverArray {
     }
   }
 
+  def mapASegSeq2[A,B](inp: Array[A], left: Int, right: Int,
+                      f: (A,A) => B, out: Array[B]) = {
+    //Writes to out(i) for left <= i <= right-1
+    var i = left
+    while(i < right) {
+      out(i) = f(inp(i))
+      i += 1
+    }
+  }
+
 }
