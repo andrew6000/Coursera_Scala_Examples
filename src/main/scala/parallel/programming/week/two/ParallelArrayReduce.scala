e76466bcbbf6a4819bbb2bb171cc26282d8d2d24
andrew6000 <ag91375@gmail.com>
2017-11-30 14:17:16 -0500

    update

--- src/main/scala/parallel/programming/week/two/ParallelArrayReduce.scala
+++ src/main/scala/parallel/programming/week/two/ParallelArrayReduce.scala
@@ -11,7 +11,7 @@ object ParallelArrayReduce {
 
     if(right - left < threshold) {
       var res = a(left)
-      var i = left + 1
+      var i = left + 1/**/
       while(i < right) {
         res = f(res, a(i))
         i += 1
@@ -42,6 +42,29 @@ object ParallelArrayReduce {
         reduceSeg(a, mid, right, f))*/
       val a1 = task{reduceSeg2(a, left, mid, f)}
       val a2 =reduceSeg2(a, mid, right, f)
+      a1.join()
+
+      f(a1.get(), a2)
+    }
+  }
+
+  def reduceSeg3[A](arr: Array[A], left: Int, right: Int,a:A,
+                    f: (A, A) => A): A = {
+
+    if(right - left < threshold) {
+      var res = arr(left)
+      var i = left + 1
+      while(i < right) {
+        res = f(res, arr(i))
+        i += 1
+      }
+      res
+    } else {
+      val mid = left + (right - left) / 2
+      /*val (a1, a2) = parallel(reduceSeg(a, left, mid, f),
+        reduceSeg(a, mid, right, f))*/
+      val a1 = task{reduceSeg2(arr, left, mid, f)}
+      val a2 =reduceSeg2(arr, mid, right, f)
       a1.join()
 
       f(a1.get(), a2)
