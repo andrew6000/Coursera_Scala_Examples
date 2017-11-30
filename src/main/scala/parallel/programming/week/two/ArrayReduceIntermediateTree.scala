6baa8f6b3c345d86351b92043554c3c6bcb402d3
andrew6000 <ag91375@gmail.com>
2017-11-30 17:05:28 -0500

    update

--- src/main/scala/parallel/programming/week/two/ArrayReduceIntermediateTree.scala
+++ src/main/scala/parallel/programming/week/two/ArrayReduceIntermediateTree.scala
@@ -9,4 +9,20 @@ object ArrayReduceIntermediateTree {
                      override val res: A,
                      r: TreeResA[A]) extends TreeResA[A]
 
+  var threshold = 1000
+
+  def upsweep[A](inp: Array[A], from: Int, to: Int,
+                 f: (A,A) => A): TreeResA[A] = {
+    if (to - from < threshold)
+      LeafA(from, to, ParallelArrayReduce.reduceSeg3(inp, from + 1, to, inp(from), f))
+    else {
+      val mid = from + (to - from)/2
+      val (tL,tR) = parallel(upsweep(inp, from, mid, f),
+        upsweep(inp, mid, to, f))
+      NodeA(tL, f(tL.res,tR.res), tR)
+    }
+  }
+
+
+
 }
