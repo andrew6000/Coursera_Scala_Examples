fdfb8a95d89b4ae96f664953def38c2c8f85c6cf
andrew6000 <ag91375@gmail.com>
2017-11-30 15:51:42 -0500

    update

--- src/test/scala/parallel/programming/week/two/ScanTreeBenchmarkTest.scala
+++ src/test/scala/parallel/programming/week/two/ScanTreeBenchmarkTest.scala
@@ -3,6 +3,7 @@ package parallel.programming.week.two
 import org.scalameter._
 
 
+
 object ScanTreeBenchmarkTest {
 
   val standardConfig = config(
@@ -12,27 +13,25 @@ object ScanTreeBenchmarkTest {
     Key.verbose -> false
   ) withWarmer(new Warmer.Default)
 
-  def plus = (x: Int, y: Int) => x + y
-  val threshold = 2000
+  val plus = (x: Int, y: Int) => x + y
+  val threshold = 20
 
-  def makeTree(len:Int): ScanTree[Int] = {
-    if (len < threshold){
-     ScanNode(ScanNode(ScanLeaf(1), ScanLeaf(3)), ScanNode(ScanLeaf(8), ScanLeaf(50)))
-    }else{
-      ScanNode(makeTree(len/2), makeTree(len - len/2))
-    }
+  def makeTree(len:Int): PrefixScan.Tree[Int] = {
+
+    PrefixScan.Node(PrefixScan.Node(PrefixScan.Leaf(1), PrefixScan.Leaf(3)), PrefixScan.Node(PrefixScan.Leaf(8), PrefixScan.Leaf(50)))
+
   }
 
   def main(args: Array[String]): Unit = {
 
-    val alen = 200000000
+    val alen = 200
     val t = makeTree(alen)
-    var t1: ScanTree[Int] = t
-    var t2: ScanTree[Int] = t
+    var t1: PrefixScan.Tree[Int] = t
+    var t2: PrefixScan.Tree[Int] = t
 
     val seqtime = standardConfig measure {
 
-      ScanTree.reduceRes(t1, plus)
+      println(PrefixScan.reduceRes(t1, plus))
     }
 
     println(s"sequential time: $seqtime ms")
