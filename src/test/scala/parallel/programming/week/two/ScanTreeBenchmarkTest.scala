e2cc06e432c03fd77ce44c7fe0f9955cc7bc5563
andrew6000 <ag91375@gmail.com>
2017-11-30 16:06:43 -0500

    update

--- src/test/scala/parallel/programming/week/two/ScanTreeBenchmarkTest.scala
+++ src/test/scala/parallel/programming/week/two/ScanTreeBenchmarkTest.scala
@@ -14,17 +14,25 @@ object ScanTreeBenchmarkTest {
   ) withWarmer(new Warmer.Default)
 
   val plus = (x: Int, y: Int) => x + y
-  val threshold = 20
+  val threshold = 700
 
   def makeTree(len:Int): PrefixScan.Tree[Int] = {
 
-    PrefixScan.Node(PrefixScan.Node(PrefixScan.Leaf(1), PrefixScan.Leaf(3)), PrefixScan.Node(PrefixScan.Leaf(8), PrefixScan.Leaf(50)))
+    if (len < threshold){
 
+      PrefixScan.Node(PrefixScan.Leaf(1),
+        PrefixScan.Node(PrefixScan.Leaf(2),
+          PrefixScan.Node(PrefixScan.Node(PrefixScan.Leaf(24), PrefixScan.Leaf(30)),
+            PrefixScan.Node(PrefixScan.Leaf(3), PrefixScan.Node(PrefixScan.Leaf(10), PrefixScan.Leaf(52))))))
+    }else{
+
+      PrefixScan.Node(makeTree(len/2), makeTree(len - len/2))
+    }
   }
 
   def main(args: Array[String]): Unit = {
 
-    val alen = 200
+    val alen = 2000
     val t = makeTree(alen)
     var t1: PrefixScan.Tree[Int] = t
     var t2: PrefixScan.Tree[Int] = t
@@ -34,7 +42,14 @@ object ScanTreeBenchmarkTest {
       println(PrefixScan.reduceRes(t1, plus))
     }
 
+    val upSweepTime = standardConfig measure {
+
+      println(PrefixScan.reduceRes(t1, plus))
+    }
+
     println(s"sequential time: $seqtime ms")
+    println(s"parallel upsweep time: $upSweepTime ms")
+    println(s"speedup time: ${ seqtime/upSweepTime}")
   }
 
 }
