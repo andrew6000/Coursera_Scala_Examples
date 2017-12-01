63fe2f7e1949eb2ed5db9070ffe90e8de7a5b9eb
andrew6000 <ag91375@gmail.com>
2017-12-01 16:16:11 -0500

    Adding squential merge sort

--- src/main/scala/parallel/programming/week/two/ParallelScanLeft.scala
+++ src/main/scala/parallel/programming/week/two/ParallelScanLeft.scala
@@ -2,7 +2,7 @@ package parallel.programming.week.two
 
 object ParallelScanLeft {
 
- def scanLeftViaMapReduce[A](inp: Array[A], a0: A, f: (A,A) => A, out: Array[A]) = {
+ def scanLeftViaMapReduce[A:Manifest](inp: Array[A], a0: A, f: (A,A) => A, out: Array[A]) = {
     val fi = (i: Int, v: A) => ParallelArrayReduce.reduceSeg3(inp, 0, i, a0, f)
     ParallelMapOverArray.mapASegPar2(inp, 0, inp.length, fi, out)
     val last = inp.length - 1
