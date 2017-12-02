d17ef7db996be90e29025933953034601809a3cc
andrew6000 <ag91375@gmail.com>
2017-12-02 17:07:10 -0500

    update

--- src/main/scala/parallel/programming/week/two/ParallelScanLeft.scala
+++ src/main/scala/parallel/programming/week/two/ParallelScanLeft.scala
@@ -3,7 +3,7 @@ package parallel.programming.week.two
 object ParallelScanLeft {
 
  def scanLeftViaMapReduce[A:Manifest](inp: Array[A], a0: A, f: (A,A) => A, out: Array[A]) = {
-    val fi = (i: Int, v: A) => ParallelArrayReduce.reduceSeg3(inp, 0, i, a0, f)
+    lazy val fi = (i: Int, v: A) => ParallelArrayReduce.reduceSeg3(inp, 0, i, a0, f)
     ParallelMapOverArray.mapASegPar2(inp, 0, inp.length, fi, out)
     val last = inp.length - 1
     out(last + 1) = f(out(last), inp(last))
